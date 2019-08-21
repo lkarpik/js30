2d953098f90f2a2856593e065572b6e4b5cb41f6
lkarpik <lkarpik@wp.pl>
2019-08-21 19:00:41 +0200

    Made it work MVP:)

--- drums/main.js
+++ drums/main.js
@@ -1,6 +1,18 @@
 let keys = document.querySelectorAll(".key");
 console.log(keys);
 
-document.addEventListener("keypress", (e) => {
-    console.log(e);
-})
+window.addEventListener("keypress", (e) => {
+    let key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    if (key) {
+        key.classList.add("playing");
+        audio.currentTime = 0;
+        audio.play();
+        window.addEventListener("keyup", (e) => {
+            let key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+            if (key) {
+                key.classList.remove("playing");
+            }
+        });
+    }
+});
