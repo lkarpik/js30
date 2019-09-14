b01b8300e048af0b81245331d7a642747c029d0a
lkarpik <lkarpik@wp.pl>
2019-09-14 22:35:59 +0200

    Ajax search

--- ajaxType/main.js
+++ ajaxType/main.js
@@ -7,3 +7,29 @@ fetch(endpoint)
     .then(result => {
         cities.push(...result);
     });
+
+const domItems = {
+    input: document.querySelector(".search"),
+    suggestions: document.querySelector(".suggestions")
+}
+
+domItems.input.addEventListener("keyup", () => {
+    search(cities, domItems.input.value);
+});
+
+function search(cities, value) {
+    const reg = new RegExp(value, "gi");
+    const matchedCities = cities.filter(city => {
+        if (city.city.match(reg)) {
+            return city;
+        }
+    });
+    console.log(matchedCities);
+    const htmlSuggestions = matchedCities.map(el => {
+        el.city.replace(reg, `<span class="hl">${value}</span>`);
+        const newEl = `<li><span>${el.city.replace(reg, `<span class="hl">${value}</span>`)}</span><span class="population">${el.population}</span></li>`;
+
+        return newEl;
+    }).join(" ");
+    domItems.suggestions.innerHTML = htmlSuggestions;
+}
