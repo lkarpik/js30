240f313bdbebc2a3e3e5d399cbf546a05b5f07f3
lkarpik <lkarpik@wp.pl>
2019-09-15 08:50:05 +0200

    Finished AjaxSearch

--- ajaxType/main.js
+++ ajaxType/main.js
@@ -1,6 +1,6 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
-let cities = []
+const cities = []
 
 fetch(endpoint)
     .then(data => data.json())
@@ -17,19 +17,25 @@ domItems.input.addEventListener("keyup", () => {
     search(cities, domItems.input.value);
 });
 
+
 function search(cities, value) {
     const reg = new RegExp(value, "gi");
     const matchedCities = cities.filter(city => {
         if (city.city.match(reg)) {
             return city;
         }
+    }).sort((a, b) => {
+        return b.population - a.population;
     });
-    console.log(matchedCities);
-    const htmlSuggestions = matchedCities.map(el => {
-        el.city.replace(reg, `<span class="hl">${value}</span>`);
-        const newEl = `<li><span>${el.city.replace(reg, `<span class="hl">${value}</span>`)}</span><span class="population">${el.population}</span></li>`;
 
+    const htmlSuggestions = matchedCities.map(el => {
+
+        const newEl = `<li><span>${el.city.replace(reg, `<span class="hl">${value}</span>`)}</span><span class="population">${parseInt(el.population).toLocaleString()}</span></li>`;
         return newEl;
     }).join(" ");
     domItems.suggestions.innerHTML = htmlSuggestions;
+};
+
+function toDecimals(digit) {
+
 }
